a8183ac8fdddab1f98480e7a24775b392bced87e
markoscapin <59868532+markoscapin@users.noreply.github.com>
2021-02-26 17:31:03 +0100

    bug fixed
    bug fixed when on the first click JS didn't respond and changed the display property of nav-social

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var button = document.querySelector("button")
 var navBarHidden = document.querySelector(".nav-social")
+
+
 button.addEventListener("click", function() {
-    if (navBarHidden.style.display == "none") {
-        navBarHidden.style.display = "flex";
+    if (navBarHidden.style.display === "flex") {
+        navBarHidden.style.display = "none"; //On first click, the display is setted by CSS. We need to check if it is already flex and turn it to none or viceversa
     } else {
-        navBarHidden.style.display = "none";
+        navBarHidden.style.display = "flex";
     }
 });
